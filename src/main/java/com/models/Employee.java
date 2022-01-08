7b0640d5cdcf6dc34972b72a7d1ad5da399a60d1
Simple Dimple John <simpledimplejohn@gmail.com>
2022-01-08 12:36:53 -0800

    initial commit

---
+++ src/main/java/com/models/Employee.java
@@ -0,0 +1,15 @@
+package com.models;
+
+
+public class Employee {
+	
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String passWord;
+	private int age;
+	private boolean active;
+	
+	
+}
