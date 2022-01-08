00f5dcb6242aa372d14b05904c9598cc058d4461
Simple Dimple John <simpledimplejohn@gmail.com>
2022-01-08 14:36:23 -0800

    update to make an Employee table

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,5 +1,6 @@
 package com.revature;
 
+import com.revature.database.MakeDatabaseCalls;
 import com.revature.inspection.ClassInspector;
 import com.revature.models.Employees;
 
@@ -7,10 +8,14 @@ public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Simple ORM Implementation");
-		
+
 		ClassInspector CI = new ClassInspector();
-		
-		CI.listFields(Employees.class);
+
+		MakeDatabaseCalls DB = new MakeDatabaseCalls();
+
+//		CI.listFields(Employees.class);
+//		System.out.println(CI.getClassTableName(Employees.class));
+		DB.MakeTableReflection(Employees.class);
 
 	}
 
