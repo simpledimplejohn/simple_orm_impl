e11758a929ac7a91fccfdf8e0ff088e8e016c290
Simple Dimple John <simpledimplejohn@gmail.com>
2022-01-08 14:03:49 -0800

    add annotations to Employees, working

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,9 +1,16 @@
 package com.revature;
 
+import com.revature.inspection.ClassInspector;
+import com.revature.models.Employees;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Simple ORM Implementation");
+		
+		ClassInspector CI = new ClassInspector();
+		
+		CI.listFields(Employees.class);
 
 	}
 
--- src/main/java/com/revature/models/Employees.java
+++ src/main/java/com/revature/models/Employees.java
@@ -4,11 +4,20 @@ import com.revature.annotations.*;
 
 @Table(tableName="employees")
 public class Employees {
+	
+	@Id()
+	@Column(columnName="emp_id")
 	private int empId;
+	@Column(columnName="first_name")
 	private String firstName;
+	@Column(columnName="last_name")
 	private String lastName;
+	@Column(columnName="user_name")
 	private String userName;
+	@Column(columnName="pass_word")
 	private String passWord;
+	@Column(columnName="age")
 	private int age;
+	@Column(columnName="active")
 	private boolean active;
 }
